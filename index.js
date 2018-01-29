4f10a312e880e4ea290384d81f6b66115c0073d9
github username <github email address>
2018-01-29 07:59:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file
+
+ function findMatching(drivers, name) {
+   return drivers.filter(function(match) {return match.charAt(0).toUpperCase() + match.slice(1) === name})
+ }
+
+function fuzzyMatch(drivers, slice) {
+  return drivers.filter(function(match) {return match.slice(0, slice.length) === slice})
+}
+
+function matchName(drivers, person) {
+  return drivers.filter(function(match) {return match["name"] === person} )
+}
